19cdbb6bf800e5777145ae218c05b9886cc6ad02
Lian <74220728+XiaoYuLian@users.noreply.github.com>
2021-01-28 21:24:32 +0800

    first commit

--- 试验场.py
+++ 试验场.py
@@ -7,7 +7,21 @@
 # b=[4,5,6]
 # print(a+b)
 # print (a*3)
-a=5/3
-b=5%3
-c=(23/10)%10
-print (a,b,c)
+
+
+# a=5/3
+# b=5%3
+# c=(23/10)%10
+# print (a,b,c)
+
+
+# a=[0,0,0,0,0]
+# b=[0,0,0,0,0]
+# for i in range (1,5):
+#     a[i]=i
+# for i in range (1,5):
+#     b[i]=2*i
+# print (a,b)
+
+n=3^3
+print (n)
